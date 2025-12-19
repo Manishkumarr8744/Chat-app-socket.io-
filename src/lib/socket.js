ed22971cd45694b5dd15b8e32d56be5fc3eb9e37
Manishkumarr8744 <mk7219630@gmail.com>
2025-12-19 20:39:06 +0530

    Refactor socket.js for improved readability

--- src/lib/socket.js
+++ src/lib/socket.js
@@ -1,6 +1,6 @@
 import {Server} from "socket.io";
 import http from "http";
-import express from "express"
+import express from "express";
 
 const app =express();
 const server= http.createServer(app);
@@ -41,3 +41,4 @@ io.on("connection",(socket)=>{
 
 
 export {io, server,app};
+
